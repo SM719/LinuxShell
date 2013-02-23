a2c7bf8240dd131bf2123c5bb1272f9d4a0530e7
zachary <zachary@u12-10>
2013-02-22 23:57:07 -0800

    Finished Path modifications, environment variable mods almost done, still broken

--- eece315_project1.c
+++ eece315_project1.c
@@ -14,11 +14,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
-int putenv(char* envString);//Removes compiler warning as fn doesn't have proper prototype
+int setenv(char* name, char* envString, int overwrite);//Removes compiler warning as fn doesn't have proper prototype
 
+void exportEnv(char* envString);
 char** getPath(int* pathArrSize);
-bool findPath(char** pathArr, int* pathArrSize, char* path);
-bool addPath(char** pathArr, int* pathArrSize, char* path);
+int findPath(char** pathArr, int* pathArrSize, char* path);
+char** addPath(char** pathArr, int* pathArrSize, char* path);
 bool removePath(char** pathArr, int* pathArrSize, char* path);
 bool setPath(char** pathArr, int pathArrSize);
 
@@ -38,9 +39,8 @@ int main(void) {
 	char* input=(char*) malloc(255);
 
 //Testing
-	char* newPath="hello";
-	//addPath(pathArr, pathArrSize, newPath);Killing it
-	setPath(pathArr,*pathArrSize);
+	char* envTest="lol=hi";
+	exportEnv(envTest);//Broken
 	if(pathArr==NULL){
 		puts("getEnv failed");
 	}else{
@@ -48,7 +48,27 @@ int main(void) {
 			puts(pathArr[i]);
 		}
 	}
-
+	char* testPath="/hi";
+	pathArr=addPath(pathArr, pathArrSize, testPath);
+	bool setSuccess=setPath(pathArr,*pathArrSize);
+	pathArr=getPath(pathArrSize);
+	if(pathArr==NULL){
+			puts("getEnv failed");
+		}else{
+			for(int i=0;i<*pathArrSize;i++){
+				puts(pathArr[i]);
+			}
+		}
+	testPath="/hello";
+	setSuccess=removePath(pathArr, pathArrSize, testPath);
+	if(pathArr==NULL){
+		puts("getEnv failed");
+	}
+	else{
+		for(int i=0;i<*pathArrSize;i++){
+			puts(pathArr[i]);
+		}
+	}
 //Code
 		scanf("%s", input);
 		if(input!=NULL){
@@ -77,11 +97,13 @@ char** getPath(int* pathArrSize){
     //Move first part of Path into array, if it doesn't exist then return.
     char* inputPath=getenv ("PATH");
     if(inputPath==NULL){
-    	return NULL;
+    	exit(EXIT_FAILURE);//GetEnv  failed
     }
-    currentPath=strtok(getenv ("PATH"), delimiter);
+    currentPath=strtok(inputPath, delimiter);
     if(currentPath!=NULL){
-    	result=(char **) malloc(sizeof (char *));
+    	if((result=(char **) malloc(sizeof (char *)))==NULL){
+    		exit(EXIT_FAILURE);//Alloc Failed
+    	}
     	result[index_currentPath]=currentPath;
     }else{
     	*pathArrSize=0;
@@ -92,7 +114,10 @@ char** getPath(int* pathArrSize){
     currentPath=strtok(NULL, delimiter);
     while(currentPath != NULL){
     	index_currentPath++;
-    	result=(char **) realloc(result, sizeof (char *)*(index_currentPath+1));//increase pointer array to hold results
+    	//increase pointer array to hold results
+    	if((result=(char **) realloc(result, sizeof (char *)*(index_currentPath+1)))==NULL){
+    		exit(EXIT_FAILURE);//Alloc Failed
+    	}
     	result[index_currentPath]=currentPath;
         currentPath=strtok(NULL, delimiter);
     }
@@ -103,54 +128,80 @@ char** getPath(int* pathArrSize){
 }
 
 /*
- * Results: path will be added to pathArr if not already their
- * Returns: True if path exists in pathArr at fn return, false otherwise
+ * Results: path will be added to pathArr if not already there
+ * Returns: pathArr
+ * Notes: modifying pathArr without returning&assigning it messes up memory somehow?-wTf
  */
-bool addPath(char** pathArr, int* pathArrSize, char* path){
+char** addPath(char** pathArr, int* pathArrSize, char* path){
 	if(path==NULL){
-		return false;
+		exit(EXIT_FAILURE);//Null path
 	}
-	if(findPath(pathArr, pathArrSize, path)){
-		return true;
+	if((findPath(pathArr, pathArrSize, path)!=-1)){
+		return pathArr;
 	}
-	pathArr=(char **) realloc(pathArr, sizeof(char *)*((*pathArrSize)+1));
+	if((pathArr=(char **) realloc(pathArr, sizeof(char *)*((*pathArrSize)+1)))==NULL){
+		exit(EXIT_FAILURE);//Alloc Failed
+	}
 	pathArr[*pathArrSize]=path;
 	*pathArrSize+=1;
-	return true;
+	return pathArr;
 }
 /*
  * Results: path will be removed from pathArr if it exists
- * Returns: True if path doesn't exist in pathArr at fn return, false otherwise
+ * Returns: True if path existed and removed, false if path didn't exist
  */
 bool removePath(char** pathArr, int* pathArrSize, char* path){
+	if(path==NULL){
+		exit(EXIT_FAILURE);//Null path
+	}
+	int index=findPath(pathArr, pathArrSize, path);
+	if(index==-1){
+		return false;
+	}
+	//Remove Path and adjust pathArr
+	for(;index<(*pathArrSize)-1;index++){
+		pathArr[index]=pathArr[index+1];
+	}
+	//Decrease the array size
+	*pathArrSize-=1;
+	//Free extra memory
+	if((pathArr=(char **) realloc(pathArr, sizeof(char *)*(*pathArrSize)))==NULL){
+		exit(EXIT_FAILURE);//Alloc Failed
+	}
 	return true;
 }
 /*
- * Returns: True if path found in pathArr, false otherwise
+ * Returns: index of path if found in pathArr, -1 otherwise
  */
-bool findPath(char** pathArr, int* pathArrSize, char* path){
-	return false;
+int findPath(char** pathArr, int* pathArrSize, char* path){
+	for(int i=0;i<*pathArrSize; i++){
+		if(strcmp(pathArr[i],path)==0)
+			return i;
+	}
+	return -1;
 }
 /*
  * Results: pathArr will be used to set the "PATH" environment variable
  * Returns: True if successful, false otherwise
+ * Notes: setPath doesn't persist between sessions
+ * http://www.cplusplus.com/forum/beginner/48228/
  */
 bool setPath(char** pathArr, int pathArrSize){
 	int pathSize=0;
 
 	//Calculate the characters from paths
 	for(int i=0;i<pathArrSize; i++){
-		pathSize+=strlen(pathArr[i]);//Fucking UP......
+		pathSize+=strlen(pathArr[i]);
 	}
 	//Calculate the characters from delimiters +5 for "PATH="
-	pathSize+=(pathArrSize-1)+5;
+	pathSize+=(pathArrSize-1);//+5;
 
 	//Allocate memory for resulting path
 	char* systemPath=malloc(pathSize);
 
 	//Build resulting path to be written
 	systemPath[0]=0;//Clear first byte so strcat always sees as empty string
-	strcat(systemPath,"PATH=");
+	//strcat(systemPath,"PATH=");
 	for(int i=0;i<pathArrSize; i++){
 		strcat(systemPath,pathArr[i]);
 		//If not the last argument then append delimiter
@@ -158,10 +209,35 @@ bool setPath(char** pathArr, int pathArrSize){
 			strcat(systemPath,":");
 		}
 	}
-	//attempt to write the path to the system and return the results
-	if(putenv(systemPath)==0){
+	//Attempt to write the path to the system and return the results
+	if((setenv("PATH",systemPath,1))==0){
 		return true;
 	}else
 		return false;
-
+}
+/*
+ * Returns: Environment variable or null if not found
+ * Note: Check if it is null before displaying
+ */
+char* getEnv(char* envName){
+	return getenv(envName);
+}
+/*
+ * Returns: Environment variable or null if not found
+ * Note: Check if it is null before displaying
+ */
+void exportEnv(char* envString){
+	char* name=malloc(sizeof(char*));
+	char* value=malloc(sizeof(char*));
+	char* delimeters=malloc(sizeof(char));
+	*delimeters='=';
+	//value=
+	//strtok(envString,delimeters);
+	//name=strtok(NULL, delimeters);
+	 char* delimiter=malloc(sizeof(char));
+	    *delimiter='=';
+	    if(envString!=NULL){
+	    	name=strtok(envString, delimiter);//Broken
+	    }
+	int x;
 }
